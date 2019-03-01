d1c5c4f3e2abeddae2658245f96e91387b92e702
arthur <arthur.teisseire@epitech.eu>
2019-03-01 14:28:33 +0100

    Add test for my_strlen

--- tests/strlen_test.c
+++ tests/strlen_test.c
@@ -10,7 +10,8 @@
 
 Test(my_strlen, basic)
 {
+    cr_assert_eq(my_strlen(""), 0);
     cr_assert_eq(my_strlen("a"), 1);
     cr_assert_eq(my_strlen("abcd"), 4);
-    cr_assert_eq(my_strlen(""), 0);
+    cr_assert_eq(my_strlen("abcdefghijklmnopqrstuvwxyz\n"), 27);
 }
