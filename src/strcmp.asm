82d41401045b0a17ad77b61b74e595ccd1979df6
arthur <arthur.teisseire@epitech.eu>
2019-03-02 16:38:04 +0100

    Refactor strcmp

--- src/strcmp.asm
+++ src/strcmp.asm
@@ -8,13 +8,6 @@ section .text
 
 strcmp:
     mov r9b, [rsi]
-    cmp [rdi], r9b
-    je equal
-    jmp not_equal
-equal:
-    mov rax, 0
-    ret
-not_equal:
     xor r10, r10
     mov r10b, [rdi]
     sub r10b, [rsi]
