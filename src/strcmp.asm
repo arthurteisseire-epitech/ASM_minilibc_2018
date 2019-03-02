734ba80e713032bb866f88e6cbe0b6a33b3e4a76
arthur <arthur.teisseire@epitech.eu>
2019-03-02 16:19:41 +0100

    Add test for my_strcmp

--- src/strcmp.asm
+++ src/strcmp.asm
@@ -7,5 +7,16 @@ global strcmp:function
 section .text
 
 strcmp:
+    mov r9b, [rsi]
+    cmp [rdi], r9b
+    je equal
+    jmp not_equal
+equal:
     mov rax, 0
+    ret
+not_equal:
+    xor r10, r10
+    mov r10b, [rdi]
+    sub r10b, [rsi]
+    movzx rax, r10b
     ret
--- tests/strcmp_test.c
+++ tests/strcmp_test.c
@@ -21,10 +21,14 @@ static int my_strcmp(const char *s1, const char *s2)
 
 static void test_strcmp(const char *s1, const char *s2)
 {
-    cr_assert_eq(my_strcmp(s1, s2), strcmp(s1, s2));
+    int mine = my_strcmp(s1, s2);
+    int other = strcmp(s1, s2);
+
+    cr_assert_eq(mine, other, "actual: %d, expected: %d", mine, other);
 }
 
 Test(my_strcmp, basic)
 {
     test_strcmp("", "");
+    test_strcmp("a", "");
 }
