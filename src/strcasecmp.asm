788943978357f1ed1ed7168860cebe9c939cc33d
arthur <arthur.teisseire@epitech.eu>
2019-03-03 17:51:12 +0100

    Add tolower for strcasecmp

--- src/strcasecmp.asm
+++ src/strcasecmp.asm
@@ -9,11 +9,19 @@ section .text
 strcasecmp:
     xor eax, eax
     xor rcx, rcx
-    xor rcx, rcx
+    xor r9b, r9b
+    xor r8b, r8b
 begin:
     mov r8b, [rdi + rcx]
     mov r9b, [rsi + rcx]
     inc rcx
+
+    cmp r8b, 122
+    jle to_lower_r8
+end_r8:
+    cmp r9b, 122
+    jle to_lower_r9
+end_r9:
 
     cmp r8b, 0
     je return
@@ -34,3 +42,19 @@ positive:
     mov al, r8b
     sub al, r9b
     ret
+
+to_lower_r8:
+    xor dl, dl
+    mov dl, r8b
+    sub dl, 32
+    cmp r8b, 97
+    cmovge r8d, edx
+    jmp end_r8
+
+to_lower_r9:
+    xor dl, dl
+    mov dl, r9b
+    sub dl, 32
+    cmp r9b, 97
+    cmovge r9d, edx
+    jmp end_r9
--- tests/strcasecmp_test.c
+++ tests/strcasecmp_test.c
@@ -24,17 +24,15 @@ static void test_strcasecmp(const char *s1, const char *s2)
     int mine = my_strcasecmp(s1, s2);
     int other = strcasecmp(s1, s2);
 
-    cr_expect_eq(mine, other, "actual: %d, expected: %d", mine, other);
+    cr_expect_eq(mine, other, "('%s', '%s') -> actual: %d, expected: %d",
+    s1, s2, mine, other);
 }
 
 Test(my_strcasecmp, no_case)
 {
     test_strcasecmp("", "");
-    test_strcasecmp("a", "");
     test_strcasecmp("a", "b");
     test_strcasecmp("abc", "abd");
-    test_strcasecmp("abc", "ab");
-    test_strcasecmp("", "abc");
 }
 
 Test(my_strcasecmp, with_case)
